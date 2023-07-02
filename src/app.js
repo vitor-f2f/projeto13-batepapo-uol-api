4e7e077cd61e59940a32d135a33e74cfb3faabee
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-07-01 22:32:24 -0300

    mudança post participants

--- src/app.js
+++ src/app.js
@@ -10,8 +10,9 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-app.listen(5000, () => {
-    console.log(`Usando porta 5000`);
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Usando porta ${PORT}`);
 });
 
 // conexão e criação do db
@@ -34,40 +35,44 @@ const participantSchema = joi.object({
 
 // requisiçoes
 app.post("/participants", async (req, res) => {
+    const participant = req.body;
+    const { error } = participantSchema.validate(participant, {
+        abortEarly: false,
+    });
+    if (error) {
+        return res.status(422).send("Erro de validação");
+    }
+
     try {
-        const { error } = participantSchema.validate(req.body);
-        if (error) {
-            return res.status(422).send("Erro de validação");
-        }
-
-        const { name } = req.body;
-
         const nameExists = await db
             .collection("participants")
-            .findOne({ name });
+            .findOne({ name: participant.name });
 
         if (nameExists) {
             return res.status(409).send("Usuario ja existe");
         }
 
         const lastStatus = Date.now();
+        const partObj = {
+            name: participant.name,
+            lastStatus: lastStatus,
+        };
+        await db.collection("participants").insertOne(partObj);
 
-        const participant = { name, lastStatus };
-        await db.collection("participants").insertOne(participant);
-
+        const msgTime = dayjs().format("HH:mm:ss");
         const message = {
-            from: name,
+            from: participant.name,
             to: "Todos",
             text: "entra na sala...",
             type: "status",
-            time: dayjs().format("HH:mm:ss"),
+            time: msgTime,
         };
         await db.collection("messages").insertOne(message);
 
         console.log("Usuario salvo com sucesso");
         return res.sendStatus(201);
     } catch (error) {
-        return res.sendStatus(500);
+        return res.status(500).send("Erro");
     }
 });
 
